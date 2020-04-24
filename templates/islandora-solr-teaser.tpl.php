c7ea4aa5307987a501cc97cfa047f17af2c013a4
rdrew <rdrew@upei.ca>
2020-04-24 11:54:03 -0300

    .

---
+++ templates/islandora-solr-teaser.tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Tempate: islandora-solr-teaser.tpl.php.
+ *
+ * @TODO: needs documentation about file and variables
+ */
+?>
+
+    <div class="islandora islandora-basic-collection-object islandora-basic-collection-list-item clearfix">
+      <dl>
+        <dt>
+          <!-- Thumbnail -->
+          <?php if (isset($values['PID'])): ?>
+            <?php print $values['thumb_link']; ?>
+          <?php endif; ?>
+        </dt>
+
+        <dd class="collection-value">
+          <?php if (isset($values['title_link'])): ?>
+            <strong><?php print filter_xss($values['title_link']); ?></strong>
+          <?php endif; ?>
+        </dd>
+        <?php if (isset($values['dc.title'])): ?>
+          <dd class="collection-value">
+            <?php print filter_xss($values['dc.title']['0']); ?>
+          </dd>
+        <?php endif; ?>
+
+
+      </dl>
+    </div>
